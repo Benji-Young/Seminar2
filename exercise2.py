8f14941c5c69359aafd2be8d17087f8b4100e751
Benji-Young <benjaminty2003@gmail.com>
2021-11-08 13:19:47 +0000

    Still working on 2

--- exercise2.py
+++ exercise2.py
@@ -6,18 +6,30 @@ matrix = [[6,-5,-7,4,-4],
 def brute_force(matrix, column, row):
     max_sum = -10000
     current_sum = 0
-    left, right = 0,0
+    max_left = 0
+    max_right = 0
+    max_up = 0
+    max_down = 0
+    left, right = 0,1
+    temp = []
+    for r in range(row):
+        temp.append(0)
 
-    for y1 in range(row):
-        for x1 in range(column):
-            for y2 in range(right, row):
-                for x2 in range(left, column):
-                    current_sum += matrix[y2][x2]
-                    if max_sum < current_sum:
-                        max_sum = current_sum
-            left += 1
-        right += 1
-            
+    for i in range(row):
+        for x1 in range(left,right):
+            for y1 in range(row):
+                temp[y1] += matrix[y1][x1]
+            right += 1 
+            current_sum = 0
+            for j in range(len(temp)):
+                current_sum += temp[j]
+                if(max_sum < current_sum):
+                    max_sum = current_sum
+                    max_down = y1
+                if(current_sum < 0):
+                    current_sum = 0
+                    max_up = y1           
+
     return max_sum
 
 print(brute_force(matrix, 5, 4))
